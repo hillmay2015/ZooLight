e00f65455920703a2626095359ed97ce3d6346d2
xym <727347070@qq.com>
2019-03-28 14:14:26 +0800

    使用模板布局layout

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -1,6 +1,9 @@
 <?php
 namespace app\admin\controller;
 
+use think\Config;
+use think\Exception;
+use think\Session;
 use think\Controller;
 
 class Index extends Controller
@@ -8,6 +11,7 @@ class Index extends Controller
     public function index()
     {
 
+        $this->view->engine->layout('layout/layout');
         return $this->fetch();
     }
 
