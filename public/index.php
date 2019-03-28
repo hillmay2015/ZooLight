bf513701b749d6c6d0bf3877ee894fa88c4a3208
xym <727347070@qq.com>
2019-03-28 09:51:21 +0800

    h-ui后台首页移到index.html文件

--- public/index.php
+++ public/index.php
@@ -12,10 +12,21 @@
 // [ 应用入口文件 ]
 namespace think;
 
+//定义站点域名
+define('SITE_HOST', $_SERVER['HTTP_HOST']);
+//带HTTP的域名
+define('HTTP_SITE_NOPATH_HOST', "http://" . SITE_HOST);
+
 // 加载基础文件
 require __DIR__ . '/../thinkphp/base.php';
 
 // 支持事先使用静态方法设置Request对象和Config对象
+// 定义应用目录
+define('APP_PATH', __DIR__ . '/../application/');
+//定义配置文件目录
+define('CONF_PATH',__DIR__.'/../config/');
+
+define('BASE_PATH',__DIR__);
 
 // 执行应用并响应
 Container::get('app')->run()->send();
