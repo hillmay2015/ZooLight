1ac9d5e9226f2749653d296e5370da9046701ee9
register820 <register820@163.com>
2019-04-02 22:02:55 +0800

    注册登录页面

---
+++ application/index/controller/Register.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lj
+ * Date: 2019/4/2
+ * Time: 9:56 PM
+ * 注册
+ */
+namespace app\index\controller;
+
+use think\Controller;
+
+class Register extends Controller
+{
+    public function index()
+    {
+        return $this->fetch();
+    }
+}
