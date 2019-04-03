8b753c551b8bde5d5c1ecd0fb15442510e985b7e
xym <727347070@qq.com>
2019-04-03 09:07:12 +0800

    config文件提交

---
+++ config/admin/app.php
@@ -0,0 +1,25 @@
+<?php
+
+
+define('CSS','/static/admin/h-ui/css/');
+define('JS','/static/admin/h-ui/js/');
+define('IMAGES','/static/admin//h-ui/images/');
+
+define('ADMIN_CSS','/static/admin/h-ui.admin/css/');
+define('ADMIN_JS','/static/admin/h-ui.admin/js/');
+define('ADMIN_IMAGES','/static/admin/h-ui.admin/images/');
+define('ADMIN_SKIN','/static/admin/h-ui.admin/skin/');
+
+define('LIB','/static/admin/lib/');
+
+return [
+    'template'=>[
+        'layout_on'=>true,
+        'layout_name'=>'layout',
+    ],
+    // 应用调试模式
+    'app_debug' => true,
+    // 应用Trace
+    'app_trace' => true,
+    'default_filter' => 'trim',
+];
