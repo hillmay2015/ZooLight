1b7c4a476db54688ade32d3fe2153e310b4e02ed
xym <727347070@qq.com>
2019-04-04 17:22:06 +0800

    分类

--- application/admin/controller/Category.php
+++ application/admin/controller/Category.php
@@ -1,7 +1,20 @@
 <?php
 namespace app\admin\controller;
 
-class Category
+class Category extends Admin
 {
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
+    public function index()
+    {
+        return $this->fetch();
+    }
+
+    public function add()
+    {
+        return $this->fetch();
+    }
 }
