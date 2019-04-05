414b8570d8257f8f820f384f0bd07743384d12b0
xym <727347070@qq.com>
2019-04-05 21:07:46 +0800

    添加分类方法

--- application/admin/controller/Category.php
+++ application/admin/controller/Category.php
@@ -35,6 +35,36 @@ class Category extends Admin
         return $this->fetch();
     }
 
+    public function CateAddDo()
+    {
+        $data['name'] = $this->request->param('name','');
+        $data['pid'] = $this->request->param('pid',0,'intval');
+
+
+        if (!empty($data['name'])) {
+            if ($data['pid'] == 0) {//添加顶级分类
+                $data['level'] = 1;
+                $re = Db::name('category')->insertGetId($data);
+                $path['path'] = '0' . ',' . $re;
+                Db::name('category')->where('id', $re)->update($path);
+                $this->success('顶级添加分类成功');
+            }
+            //添加子分类
+            $res = Db::name('category')->field('path')->find($data['pid']);
+            $data['path'] = $res['path'];
+            $data['level'] = substr_count($data['path'], ',');
+            $re = Db::name('category')->insertGetId($data);
+            $path['path'] = $data['path'] . ',' . $re;
+            $path['level'] = substr_count($path['path'], ',');
+            Db::name('category')->where('id', $re)->update($path);
+            $this->success('添加分类成功');
+
+        } else {
+            $this->error('请填写分类名称');
+        }
+
+    }
+
     public function getCateGory()
     {
         $category = $this->model->getlist('', 'id,pid as pId,name', '', false);
