abc1ed35c07d896b380c87c8b0f50a09b3b210d5
xym <727347070@qq.com>
2019-04-06 17:16:53 +0800

    产品添加页面

--- application/admin/controller/Product.php
+++ application/admin/controller/Product.php
@@ -2,15 +2,31 @@
 namespace app\admin\controller;
 
 use app\common\traits\CategoryTraits;
+use think\validate;
+use app\admin\model\Product as ProductModel;
 
 class Product extends Admin
 {
     use CategoryTraits;
 
+    protected $rule = [
+        'admin_name' => 'require',
+        'password' => 'require',
+        'captcha' => 'require',
+    ];
+
+    protected $message = [
+        'admin_name.require' => '请输入登录账号',
+        'password.require' => '请输入登录密码',
+        'captcha.require' => '验证码不能为空',
+    ];
+
     public function __construct()
     {
         parent::__construct();
+        $this->model = new ProductModel();
     }
+
 
     public function productList()
     {
@@ -23,6 +39,35 @@ class Product extends Admin
         $data = $this->cateAll();
         $this->assign('data', $data);
         return $this->fetch();
+    }
+
+    public function doAdd()
+    {
+        $data = $this->request->param();
+        $this->check($data);
+        echo '<pre>';
+        print_r($data);
+        exit();
+        $res=$this->model->insert($data);
+        if($res){
+            $this->success('添加产品成功');
+        }else{
+            $this->error('添加产品失败');
+        }
+
+
+    }
+
+    /**
+     * 验证数据
+     */
+    protected function check($data)
+    {
+        $validate = new Validate($this->rule, $this->message);
+        $result = $validate->check($data);
+        if (!$result) {
+            $this->error($validate->getError());
+        }
     }
 
 }
---
+++ application/admin/model/Product.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * User: may
+ * Date: 2019/4/6
+ * Time: 下午4:59
+ * 产品相关操作model
+ */
+namespace app\admin\model;
+
+use think\Model;
+
+class Product extends Model
+{
+}
