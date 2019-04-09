867ca52fea4ace152329a2034fe453b2c3c12f2a
xym <727347070@qq.com>
2019-04-09 17:20:18 +0800

    首页图片输出

--- config/index/app.php
+++ config/index/app.php
@@ -8,6 +8,7 @@
 define('CSS','/static/index/css/');
 define('JS','/static/index/js/');
 define('IMAGE','/static/index/image/');
+define('ADMIN_IMAGE','/static/admin/lib/webuploader/0.1.5/server/upload/');
 return [
     'template'=>[
         'layout_on'=>true,
