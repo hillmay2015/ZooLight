42643ae4a795e41d71a0e0f8ebc4a7d2fdfc29ef
xym <727347070@qq.com>
2019-06-13 15:29:52 +0800

    数据库操作封装

--- application/common/traits/DbTraits.php
+++ application/common/traits/DbTraits.php
@@ -11,10 +11,85 @@ use Think\db;
 
 trait DbTraits
 {
+    /**
+     * @param $where
+     * @param string $field
+     * @param string $order
+     * @return mixed
+     * 查询所有列表
+     */
+    public function selectAllList($where, $field = '*', $order = 'id desc')
+    {
+        $result = $this->where($where)->order($order)->select();
+        return $result;
+    }
 
+    /**
+     * @param $where
+     * @param string $field
+     * @return mixed
+     * 查询单条数据信息
+     */
     public function findOneList($where, $field = '*')
     {
         $result = $this->where($where)->find();
+        return $result;
+    }
+
+    /**
+     * @param $where
+     * @return mixed
+     * 统计数据条数
+     */
+    public function getCount($where)
+    {
+        $result = $this->where($where)->Count();
+        return $result;
+    }
+
+    /**
+     * @param $where
+     * @param $field
+     * @return mixed
+     * 统计数据总量
+     */
+    public function getSum($where, $field)
+    {
+        $result = $this->where($where)->Sum($field);
+        return $result;
+    }
+
+    /**
+     * @param $where
+     * @param $data
+     * @return mixed
+     * 更新数据
+     */
+    public function update($where, $data)
+    {
+        $result = $this->where($where)->update($data);
+        return $result;
+    }
+
+    /**
+     * @param $where
+     * @return mixed
+     * 删除数据
+     */
+    public function delete($where)
+    {
+        $result = $this->where($where)->delete();
+        return $result;
+    }
+
+    /**
+     * @param $data
+     * @return mixed
+     * 插入数据
+     */
+    public function insert($data)
+    {
+        $result = $this->insertData($data);
         return $result;
     }
 }
