483f1c66b0dc905e3548ae921f4ce34aaf0d6437
xym <727347070@qq.com>
2019-06-14 11:28:54 +0800

    通知

---
+++ application/index/controller/Notify.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * User: may
+ * Date: 2019/6/14
+ * Time: 上午11:07
+ * 支付宝支付后回调
+ */
+namespace app\index\controller;
+
+use think\Controller;
+
+class Notify extends Controller
+{
+    //异步通知
+    public function alipayNotify()
+    {
+
+    }
+
+    //同步通知
+    public function alipayReturn()
+    {
+
+    }
+
+    //更新订单状态
+    public function updateOrder()
+    {
+
+    }
+
+}
